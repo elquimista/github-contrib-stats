89c6a0b76241edab8bbc7a0b723de3d39390ccc1
darkturtle <dark.turtle@protonmail.com>
2017-03-15 12:10:02 +0000

    Feat: add streak stats

--- index.js
+++ index.js
@@ -7,63 +7,78 @@ const moment = require('moment');
 
 // isoDate()
 const isoDate = date => moment(date).format('YYYY-MM-DD');
+// getBasicUserData()
+const getBasicUserData = async username => (await requestify.get(`https://api.github.com/users/${username}`)).getBody();
 
-// getUserJoinedDate()
-async function getUserJoinedDate(username) {
-  const res = await requestify.get(`https://api.github.com/users/${username}`);
-  return res.getBody().created_at;
-}
-
-// getContributionsCount()
-async function getContributionCount(username, toDate) {
+// getContributionss()
+async function getContributions(username, toDate) {
   const res = await requestify.get(`https://github.com/users/${username}/contributions?to=${isoDate(toDate)}`);
   const $ = cheerio.load(res.getBody());
   const fromDate = moment(toDate).subtract(1, 'y');
+  const data = [];
   let contributions = 0;
+  let date;
+  let count;
 
   $('.day').each((_, e) => {
-    if (moment($(e).data('date')).unix() > fromDate.unix()) {
-      contributions += parseInt($(e).data('count'), 10);
+    date = $(e).data('date');
+    count = parseInt($(e).data('count'), 10);
+    if (moment(date).unix() > fromDate.unix()) {
+      data.push({ date, count });
+      contributions += count;
     }
   });
 
-  return contributions;
+  return { data, count: contributions };
 }
 
-// getTotalContributionsCount()
-async function getTotalContributionCount(username) {
-  const joinedDate = moment(await getUserJoinedDate(username)).startOf('d');
+// getStreaks()
+function getStreaks(data) {
+  const endDate = data[data.length - 1].date;
+  let currentStreak = 0;
+  let longestStreak = 0;
+
+  for (const datum of data) {
+    if (datum.count > 0) {
+      currentStreak += 1;
+      if (currentStreak > longestStreak) {
+        longestStreak = currentStreak;
+      }
+    }
+    else if (datum.date !== endDate) {
+      currentStreak = 0;
+    }
+  }
+
+  return { currentStreak, longestStreak };
+}
+
+// getContributionStats()
+async function getContributionStats(username, userJoinedDate) {
+  const joinedDate = moment(userJoinedDate).startOf('d');
   let toDate = moment(new Date()).startOf('d');
   let totalContributions = 0;
+  let contributions;
+  let data = [];
 
   while (toDate.unix() > joinedDate.unix()) {
-    totalContributions += await getContributionCount(username, toDate);
+    contributions = await getContributions(username, toDate);
+    data = contributions.data.concat(data);
+    totalContributions += contributions.count;
     toDate = toDate.subtract(1, 'y').startOf('d');
   }
 
-  return totalContributions;
-}
+  const { currentStreak, longestStreak } = getStreaks(data);
+  const todaysContributions = data[data.length - 1].count;
 
-// getTodaysContributionCount()
-async function getTodaysContributionCount(username) {
-  const res = await requestify.get(`https://github.com/users/${username}/contributions`);
-  const $ = cheerio.load(res.getBody());
-  const $dayLastEl = $('.day').last();
-
-  if (moment($dayLastEl.data('date')).unix() === moment().startOf('d').unix()) {
-    return parseInt($dayLastEl.data('count'), 10);
-  } else {
-    return 0;
-  }
+  return { totalContributions, todaysContributions, currentStreak, longestStreak };
 }
 
 // exports
 module.exports = async username => {
   try {
-    return {
-      totalContributions: await getTotalContributionCount(username),
-      todaysContributions: await getTodaysContributionCount(username)
-    };
+    const basicUserData = await getBasicUserData(username);
+    return await getContributionStats(username, basicUserData.created_at);
   }
   catch (err) {
     console.error(err, err.stack);
