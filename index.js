b56402c0b4843f56993c9c54687e1e4e415b4225
darkturtle <dark.turtle@protonmail.com>
2017-03-15 11:31:34 +0000

    Feat: add todaysContributions

--- index.js
+++ index.js
@@ -8,13 +8,14 @@ const moment = require('moment');
 // isoDate()
 const isoDate = date => moment(date).format('YYYY-MM-DD');
 
+// getUserJoinedDate()
 async function getUserJoinedDate(username) {
   const res = await requestify.get(`https://api.github.com/users/${username}`);
   return res.getBody().created_at;
 }
 
 // getContributionsCount()
-async function getContributionsCount(username, toDate) {
+async function getContributionCount(username, toDate) {
   const res = await requestify.get(`https://github.com/users/${username}/contributions?to=${isoDate(toDate)}`);
   const $ = cheerio.load(res.getBody());
   const fromDate = moment(toDate).subtract(1, 'y');
@@ -30,24 +31,38 @@ async function getContributionsCount(username, toDate) {
 }
 
 // getTotalContributionsCount()
-async function getTotalContributionsCount(username) {
+async function getTotalContributionCount(username) {
   const joinedDate = moment(await getUserJoinedDate(username)).startOf('d');
   let toDate = moment(new Date()).startOf('d');
   let totalContributions = 0;
 
   while (toDate.unix() > joinedDate.unix()) {
-    totalContributions += await getContributionsCount(username, toDate);
+    totalContributions += await getContributionCount(username, toDate);
     toDate = toDate.subtract(1, 'y').startOf('d');
   }
 
   return totalContributions;
 }
 
+// getTodaysContributionCount()
+async function getTodaysContributionCount(username) {
+  const res = await requestify.get(`https://github.com/users/${username}/contributions`);
+  const $ = cheerio.load(res.getBody());
+  const $dayLastEl = $('.day').last();
+
+  if (moment($dayLastEl.data('date')).unix() === moment().startOf('d').unix()) {
+    return parseInt($dayLastEl.data('count'), 10);
+  } else {
+    return 0;
+  }
+}
+
 // exports
 module.exports = async username => {
   try {
     return {
-      totalContributions: await getTotalContributionsCount(username)
+      totalContributions: await getTotalContributionCount(username),
+      todaysContributions: await getTodaysContributionCount(username)
     };
   }
   catch (err) {
